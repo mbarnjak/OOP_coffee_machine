21cd937879717c83d351d8789ad1d0ec778a3dd2
mbarnjak <barnjakmartin@gmail.com>
2024-11-07 14:02:57 +0100

    Initial commit

---
+++ src/Logic.java
@@ -0,0 +1,139 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Logic {
+    int mlOfWater;
+    int mlOfMilk;
+    int gOfCoffee;
+    int cups;
+    int money;
+    String username;
+    String password;
+
+
+    String path = "doc/Ingredients.txt";
+
+
+    public void updateIngredients(){
+        try (PrintWriter writer = new PrintWriter(new FileWriter(path))){
+            writer.print(mlOfWater + "; " + mlOfMilk + "; " + gOfCoffee + "; " + cups +  "; " + money +"\nadmin; admin123" );
+        }catch (Exception e){
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+
+    public void getIngredients(){
+        Scanner scanner;
+
+        try (FileReader filereader = new FileReader(path)){
+            scanner = new Scanner(filereader);
+
+            scanner.useDelimiter("; |\n");
+
+            mlOfWater = scanner.nextInt();
+            mlOfMilk = scanner.nextInt();
+            gOfCoffee = scanner.nextInt();
+            cups = scanner.nextInt();
+            money = scanner.nextInt();
+
+            username = scanner.next();
+            password = scanner.next();
+            System.out.println(username);
+
+        }catch (IOException e){
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+    }
+
+
+    public void fillMachineWithIngredients(int waterToAdd, int milkToAdd, int coffeeToAdd, int cupsToAdd) {
+        mlOfWater += waterToAdd;
+
+        mlOfMilk += milkToAdd;
+
+        gOfCoffee += coffeeToAdd;
+
+        cups += cupsToAdd;
+    }
+
+
+    public void takeMoney() {
+        System.out.println("I gave you $" + money );
+        money = 0;
+
+    }
+
+
+    //espresso
+    public boolean enoughResourcesEspresso() {
+        if (mlOfWater < 250) {
+            return false;
+        }
+        else if (gOfCoffee < 16) {
+            return false;
+        }
+        else return cups >= 1;
+    }
+
+    public void makeEspresso(){
+        mlOfWater -= 250;
+        gOfCoffee -= 16;
+        cups -= 1;
+        money += 4;
+    }
+
+
+    //latte
+    public boolean enoughResourcesLatte() {
+        if (mlOfWater < 350) {
+            return false;
+        }else if(mlOfMilk < 75){
+            return false;
+        }
+        else if (gOfCoffee < 20) {
+            return false;
+        }
+        else return cups >= 1;
+    }
+
+    public void makeLatte(){
+        mlOfWater -= 350;
+        mlOfMilk -= 75;
+        gOfCoffee -= 20;
+        cups -= 1;
+        money += 7;
+    }
+
+
+    //cappuccino
+    public boolean enoughResourcesCappuccino() {
+        if (mlOfWater < 200) {
+            return false;
+        }else if(mlOfMilk < 100){
+            return false;
+        }
+        else if (gOfCoffee < 12) {
+            return false;
+        }
+        else return cups >= 1;
+    }
+
+    public void makeCappuccino(){
+        mlOfWater -= 200;
+        mlOfMilk -= 100;
+        gOfCoffee -= 12;
+        cups -= 1;
+        money += 6;
+    }
+
+
+
+
+
+
+}
---
+++ src/UI.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+
+public class UI {
+
+    Scanner scanner = new Scanner(System.in);
+
+    Logic logic = new Logic();
+
+
+    public void startMachine() {
+        logic.getIngredients();
+        while (true) {
+            System.out.println("1- buy\n2- login");
+            System.out.print(">");
+
+            int choice = inputInt();
+
+
+            //buy
+            if (choice == 1) {
+                chooseCoffee();
+            } else if (choice == 2) {
+                System.out.println("Enter username:");
+                System.out.print(">");
+                String userNameInput = scanner.next();
+                System.out.println("Enter password:");
+                System.out.print(">");
+                String passwordInput = scanner.next();
+                if (userNameInput.equals(logic.username) && passwordInput.equals(logic.password)) {
+                    while (true) {
+                        printMainMenu();
+                        choice = inputInt();
+                        if (choice == 1) {
+                            System.out.println("How many ml of water you want to add:");
+                            int water = inputInt();
+
+                            System.out.println("How many ml of milk you want to add:");
+                            int milkToAdd = inputInt();
+
+                            System.out.println("How many coffee you want to add?");
+                            int coffee = inputInt();
+
+                            System.out.println("How many cups you want to add?");
+                            int cupsToAdd = inputInt();
+
+                            logic.fillMachineWithIngredients(water, milkToAdd, coffee, cupsToAdd);
+
+                            //take
+                        } else if (choice == 2) {
+                            logic.takeMoney();
+
+                            //remaining
+                        } else if (choice == 3) {
+                            printIngredients();
+                        }
+
+                        //exit
+                        else if (choice == 4) {
+                            System.out.println("Turning off...");
+                            logic.updateIngredients();
+                            System.exit(0);
+                        } else if (choice == 5) {
+                            break;
+                        }
+                    }
+                }else System.out.println("Wrong username or password");
+                //wrong input
+            } else System.out.println("Wrong input");
+
+        }
+    }
+
+
+    private void chooseCoffee() {
+
+        printCoffeeMenu();
+
+
+        int choice = inputInt();
+
+        if (choice == 1) {
+            if (logic.enoughResourcesEspresso()) {
+                logic.makeEspresso();
+                System.out.println("I have enough resources, making you a espresso!");
+            } else printNotEnoughResources();
+        } else if (choice == 2) {
+            if (logic.enoughResourcesLatte()) {
+                logic.makeLatte();
+                System.out.println("I have enough resources, making you a latte!");
+            } else printNotEnoughResources();
+        } else if (choice == 3) {
+            if (logic.enoughResourcesCappuccino()) {
+                logic.makeCappuccino();
+                System.out.println("I have enough resources, making you a Cappuccino!");
+            } else printNotEnoughResources();
+        } else if (choice == 4) {
+            System.out.println(" ");
+        } else {
+            printWrongInput();
+        }
+
+    }
+
+
+    private void printIngredients() {
+        System.out.println("The coffee machine has: \n" + logic.mlOfWater + " ml of water\n" + logic.mlOfMilk + " ml of milk\n" + logic.gOfCoffee +
+                " g of coffee beans\n" + logic.cups + " disposable cups\n" + "$" + logic.money + " of money");
+    }
+
+
+    private void printNotEnoughResources() {
+        System.out.println("Not enough resources.");
+    }
+
+
+    private void printMainMenu() {
+        System.out.println("Write action(1 - fill, 2 - take, 3 - remaining, 4 - turn off, 5 - exit):");
+        System.out.print(">");
+    }
+
+
+    private void printCoffeeMenu() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, 4 - main menu:");
+        System.out.print(">");
+    }
+
+
+    private void printWrongInput() {
+        System.out.println("Wrong input");
+    }
+
+
+    private int inputInt() {
+        if (scanner.hasNextInt())
+            return scanner.nextInt();
+        else {
+            scanner.next();
+            return 0;
+        }
+    }
+
+
+}
