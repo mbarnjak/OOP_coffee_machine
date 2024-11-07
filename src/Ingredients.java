7e6029a23d24387c12daa060c14551981cafc0ae
mbarnjak <barnjakmartin@gmail.com>
2024-11-07 14:59:38 +0100

    Add class Ingredients

---
+++ src/Ingredients.java
@@ -0,0 +1,65 @@
+public class Ingredients {
+    private int mlOfWater;
+    private int mlOfMilk;
+    private int gOfCoffee;
+    private int cups;
+    private int money;
+    private String username;
+    private String password;
+
+    public int getMlOfWater() {
+        return mlOfWater;
+    }
+
+    public void setMlOfWater(int mlOfWater) {
+        this.mlOfWater = mlOfWater;
+    }
+
+    public int getMlOfMilk() {
+        return mlOfMilk;
+    }
+
+    public void setMlOfMilk(int mlOfMilk) {
+        this.mlOfMilk = mlOfMilk;
+    }
+
+    public int getgOfCoffee() {
+        return gOfCoffee;
+    }
+
+    public void setgOfCoffee(int gOfCoffee) {
+        this.gOfCoffee = gOfCoffee;
+    }
+
+    public int getCups() {
+        return cups;
+    }
+
+    public void setCups(int cups) {
+        this.cups = cups;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
